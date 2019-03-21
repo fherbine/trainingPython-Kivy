290d79f5b37f1857c7af5b4a16b63f2d6ad5e0e5
fherbine <felix.herbinet@yahoo.com>
2019-03-22 00:28:45 +0100

    ZStr basic repr test

--- Python/OC/z_str.py
+++ Python/OC/z_str.py
@@ -1,2 +1,10 @@
 class ZStr():
-    pass
+    def __init__(self, str_base=''):
+        self._str_base = ''
+
+    def __repr__(self):
+        return '\'\''
+
+
+def test_basic_repr():
+    assert repr(str()) == repr(ZStr())
