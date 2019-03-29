691b0ece1f149552880d8e4d90ae4785a6be5299
fherbine <felix.herbinet@yahoo.com>
2019-03-29 18:30:51 +0100

    start/stop/step

--- Python/OC/x_range.py
+++ Python/OC/x_range.py
@@ -1,10 +1,10 @@
-def x_range(start_stop, stop=None, step=None):
+def x_range(start_stop, stop=None, step=1):
     yielded = 0 if stop is None else start_stop
     ystop = start_stop if stop is None else stop
 
     while yielded < ystop:
         yield yielded
-        yielded += 1
+        yielded += step
 
 def test_basic_range0():
     assert [_ for _ in range(0)] == [_ for _ in x_range(0)]
@@ -17,6 +17,12 @@ def test_long_range():
 
 def test_startstop_long_range():
     assert [_ for _ in range(40, 100)] == [_ for _ in x_range(40, 100)]
+
+def test_startstopstep_basic_long_range():
+    assert [_ for _ in range(40, 100, 1)] == [_ for _ in x_range(40, 100, 1)]
+
+def test_startstopstep_long_range():
+    assert [_ for _ in range(40, 100, 10)] == [_ for _ in x_range(40, 100, 10)]
 
 if __name__ == '__main__':
     pass
