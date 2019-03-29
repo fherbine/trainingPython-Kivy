bfeda3e444d071d2f99774e65ece1082389dc6e1
fherbine <felix.herbinet@yahoo.com>
2019-03-29 17:18:07 +0100

    base string (multiple chars test)

--- Python/OC/instr.py
+++ Python/OC/instr.py
@@ -1,11 +1,14 @@
 class InStr():
     """ InStr: just an infinite str """
 
+    def __init__(self, base_string=''):
+        self.base_str = base_string
+
     def __repr__(self):
-        return "''"
+        return "'{}'".format(self.base_str)
 
     def __str__(self):
-        return ''
+        return '{}'.format(self.base_str)
 
 
 def test_basic_repr():
@@ -13,6 +16,12 @@ def test_basic_repr():
 
 def test_basic_str():
     assert str(str()) == str(InStr())
+
+def test_chars_str_repr():
+    assert repr(str('toto')) == repr(InStr('toto'))
+
+def test_chars_str_str():
+    assert str(str('toto')) == str(InStr('toto'))
 
 if __name__ == '__main__':
     pass
