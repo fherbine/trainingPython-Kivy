ffeff2b5c469a33be13eb3d81e0f479cc8c842f8
fherbine <felix.herbinet@yahoo.com>
2019-03-29 17:08:40 +0100

    Basic str / repr tests

--- Python/OC/instr.py
+++ Python/OC/instr.py
@@ -1,3 +1,18 @@
 class InStr():
     """ InStr: just an infinite str """
+
+    def __repr__(self):
+        return "''"
+
+    def __str__(self):
+        return ''
+
+
+def test_basic_repr():
+    assert repr(str()) == repr(InStr())
+
+def test_basic_str():
+    assert str(str()) == str(InStr())
+
+if __name__ == '__main__':
     pass
