a67d227931fe36c2aa13b02be3f974684936d2ab
fherbine <felix.herbinet@yahoo.com>
2019-03-29 17:21:19 +0100

    Str casts tests

--- Python/OC/instr.py
+++ Python/OC/instr.py
@@ -23,5 +23,17 @@ def test_chars_str_repr():
 def test_chars_str_str():
     assert str(str('toto')) == str(InStr('toto'))
 
+def test_str_int():
+    assert str(str(3)) == str(InStr(3))
+
+def test_str_float():
+    assert str(str(3.6)) == str(InStr(3.6))
+
+def test_str_list():
+    assert str(str(list())) == str(InStr(list()))
+
+def test_str_dict():
+    assert str(str(dict())) == str(InStr(dict()))
+
 if __name__ == '__main__':
     pass
