13b3f17c78b3ca784620ce9e015267c59822b94f
fherbine <felix.herbinet@yahoo.com>
2019-03-29 18:13:18 +0100

    Basic test range(0)

--- Python/OC/x_range.py
+++ Python/OC/x_range.py
@@ -1,5 +1,8 @@
 def x_range(start_stop, stop=None, step=None):
     pass
 
+def test_basic_range0():
+    assert [_ for _ in range(0)] == [_ for _ in range(0)]
+
 if __name__ == '__main__':
     pass
