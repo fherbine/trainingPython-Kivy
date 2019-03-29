cfd62073095968f792061ad287af674dae040bf0
fherbine <felix.herbinet@yahoo.com>
2019-03-29 18:26:22 +0100

    Long test range

--- Python/OC/x_range.py
+++ Python/OC/x_range.py
@@ -1,12 +1,18 @@
 def x_range(start_stop, stop=None, step=None):
+    yielded = 0
     if start_stop:
-        yield 0
+        while yielded < start_stop:
+            yield yielded
+            yielded += 1
 
 def test_basic_range0():
     assert [_ for _ in range(0)] == [_ for _ in x_range(0)]
 
 def test_basic_range1():
     assert [_ for _ in range(1)] == [_ for _ in x_range(1)]
+
+def test_long_range():
+    assert [_ for _ in range(100)] == [_ for _ in x_range(100)]
 
 if __name__ == '__main__':
     pass
