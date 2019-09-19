b70a11aafb53a87f6349db65791bcba4173a2205
fherbine <felix.herbinet@yahoo.com>
2019-09-20 00:46:12 +0200

    basis for game map controller

---
+++ Python/OC/activity2/server_controllers/game_map.py
@@ -0,0 +1,23 @@
+class GameMap:
+    map_buffer = ''
+    elements = []
+
+    def __init__(self, path):
+        self.load_map(path)
+
+    def load_map(self, path):
+        pass
+
+    def add_player(self, skin):
+        pass
+
+    def __str__(self):
+        self.update_map_buffer()
+
+        return map_buffer
+
+    def _update_map_buffer(self):
+        return
+
+    def _read_map_file(self):
+        pass
