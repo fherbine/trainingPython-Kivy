0d7b4e8522095c47e7e8b54e923019ef32aee431
fherbine <felix.herbinet@yahoo.com>
2019-09-24 17:53:54 +0200

    Base for map drawings

--- algorithms/astar_pathfinder/screens/path_finder.py
+++ algorithms/astar_pathfinder/screens/path_finder.py
@@ -11,9 +11,16 @@ import widgets.tiles as tiles_classes
 
 class PathFinderScreen(Screen):
     tiles_type = StringProperty()
+    action = StringProperty()
     tiles = ListProperty()
     tile_class = ObjectProperty(allownone=True)
     container = ObjectProperty(allonone=True)
+
+    action_colors = {
+        'arrival': (.2, 1, .2, 1),
+        'departure': (1, .2, .2, 1),
+        'wall': (0, 0, 0, 1),
+    }
 
     def on_tiles_type(self, *_):
         if not self.tiles_type:
@@ -30,7 +37,17 @@ class PathFinderScreen(Screen):
             self.container.add_widget(tile)
 
     def eval_tile(self, tile):
-        print(tile, tile in self.tiles)
+        if tile not in self.tiles:
+            return
+
+        tile.color = self.action_colors[self.action]
+
+        if self.action in ('arrival', 'departure'):
+            self.action = ''
+
+    def reset(self):
+        self.container.clear_widgets()
+        self.fill_map()
 
 
 Builder.load_file('screens/path_finder.kv')
