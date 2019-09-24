9f329c48b986feb9a9853a158d03c3cd17d0d266
fherbine <felix.herbinet@yahoo.com>
2019-09-24 18:44:56 +0200

    Settings file

--- algorithms/astar_pathfinder/main.py
+++ algorithms/astar_pathfinder/main.py
@@ -1,13 +1,24 @@
+import json
+
 from kivy.app import App
-from kivy.properties import ObjectProperty
+from kivy.properties import (
+    BooleanProperty,
+    ObjectProperty,
+)
 
 from screens import * # noqa
 from widgets import * # noqa
 
 class AstarApp(App):
     screenmanager = ObjectProperty(rebind=True)
+    diagonals = BooleanProperty(False)
 
     def build(self):
+        with open('settings.json') as settings_file:
+            settings = json.load(settings_file)
+
+        self.diagonals = settings.get('diagonals', False)
+
         self.screenmanager = self.root.screenmanager
 
 if __name__ == '__main__':
